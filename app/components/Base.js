807853b1fb3a343ffbe20ae07f3b5085362fd69b
Edmund Korley <korley.em@gmail.com>
2016-08-10 16:13:15 -0700

    Fetching from GraphQL endpoint

--- app/components/Base.js
+++ app/components/Base.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-import Relay from 'react-relay';
 import Header from './Header';
 import Map from './Map';
 import Query from './Query';
 import Result from './Result';
 import Footer from './Footer';
 
+// Our fetch module for making simple queries to our GraphQL API
+var fetch = require('graphql-fetch')('http://localhost:8000/graphql')
+
 class Base extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    let query = `
+        query {
+            businesses {
+                id, name, description, category, address, latitude, longitude
+            }
+        }
+    `;
+
+    fetch(query)
+          .then((results) => {
+            if (results.errors) console.warn('****** ERRORS', results.errors);
+
+              console.log(`RESULTS ****`);
+              console.log(results);
+              return results;
+          })
+          .catch((error) => {
+            console.warn(`****** ERRORS ${error}`);
+          });
+  }
+  render() {
     console.log(`****** PROPS ${JSON.stringify(this.props, null, '\t')}`);
     return <div className="container">
       <Header />
@@ -28,12 +55,5 @@ class Base extends Component {
   }
 }
 
-export default Relay.createContainer(Base, {
-  fragments: {
-    businesses: () => Relay.QL`
-      fragment on Business @relay(plural: true) {
-        id, name, description, category, address, latitude, longitude
-      }
-    `,
-  }
-});
+export default Base;
+
