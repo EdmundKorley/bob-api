9e1e248da13cdc222e08ff3795ba1af6290d8289
Edmund Korley <korley.em@gmail.com>
2016-08-10 16:12:32 -0700

    Sans relay

--- app/index.js
+++ app/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Relay from 'react-relay';
 import Base from './components/Base';
-import BaseRoute from './routes/BaseRoute';
 
 // Where our React app touches our HTML
 const entryDiv = document.getElementById('bob-portal')
 
-render(
-  <Relay.RootContainer Component={Base} route={new BaseRoute()} />,
-  entryDiv);
+render(<Base/>, entryDiv);
